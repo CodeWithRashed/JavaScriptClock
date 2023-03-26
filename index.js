348cd7b128e72c46d7154453492d6bd6f679bfd2
deve-rashed <tools.rashed@gmail.com>
2023-03-26 12:55:09 +0600

    added code to make the clock working

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-window.addEventListener('load', function() {
+function clock(){
 
-});
+//getting informations//
+var today = new Date;
+var hour = today.getHours(); 
+var min = today.getMinutes();
+var sec = today.getSeconds();
+//days and months//
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var date = today.getDate();
+var day = daysOfWeek[today.getDay()]
+var month = monthsOfYear[today.getMonth()]
+var year = today.getFullYear();
+
+//adding information to html file//
+document.getElementById("hr").innerHTML = hour 
+document.getElementById("min").innerHTML = min
+document.getElementById("sec").innerHTML = sec
+document.getElementById("day").innerHTML = day + ","
+document.getElementById("today").innerHTML = date
+document.getElementById("month").innerHTML = month
+document.getElementById("year").innerHTML = year
+
+}
+
+setInterval(clock, 1000);
